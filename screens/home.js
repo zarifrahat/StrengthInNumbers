83eedfbc0dfe114db0b418ad42d2544ef4f1239d
zrahat <zarifrahat123@gmail.com>
2020-08-09 22:51:08 -0400

    Add homescreen

---
+++ screens/home.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, Image, View, SafeAreaView, TouchableOpacity, Button, TextInput } from 'react-native';
+
+
+export default function Home() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>You are on the Home screen</Text>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
