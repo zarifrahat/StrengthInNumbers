74249f3c4e55ed21fd0372164743d6e9029e8741
zrahat <zarifrahat123@gmail.com>
2020-08-11 20:47:33 -0400

    Add workout details page

--- screens/workoutDetails.js
+++ screens/workoutDetails.js
@@ -3,11 +3,29 @@ import { StyleSheet, Text, SafeAreaView, TouchableOpacity } from "react-native";
 import Card from "../shared/card";
 
 export default function WorkoutDetails({ navigation }) {
+    
+  console.log(navigation.getParam('days'))
+  const listedDays = navigation.getParam('days').map(ele => {
+      return (
+        <Text>
+          {ele}
+        </Text>
+      )
+    });
+
+  const allWorkouts = navigation.getParam('loggedWorkouts');
+  const lastWorkout= allWorkouts[allWorkouts.length-1]
+
   return (
     <SafeAreaView style={styles.container}>
+      <Text>Last workout logged: {lastWorkout.day}</Text>
+      <Text>on {lastWorkout.dateLogged}</Text>
+      <Text>Today is {}</Text>
         <Card>
             <Text>{navigation.getParam('title')}</Text>
-            <Text>{navigation.getParam('days')}</Text>
+            <SafeAreaView>
+                {listedDays}
+            </SafeAreaView>
         </Card>
     </SafeAreaView>
   );
