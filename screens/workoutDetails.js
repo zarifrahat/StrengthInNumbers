d056d35340ac6e738ffb0a30fe6afa356f3a80f9
zrahat <zarifrahat123@gmail.com>
2020-08-11 21:27:09 -0400

    Add workout details page

--- screens/workoutDetails.js
+++ screens/workoutDetails.js
@@ -4,18 +4,29 @@ import Card from "../shared/card";
 
 export default function WorkoutDetails({ navigation }) {
   const daysArray = navigation.getParam('days');
-  const listedDays = daysArray.map(ele => {
-      return (
-        <Text>
-          {ele}
-        </Text>
-      )
-    });
-
+  
   const allWorkouts = navigation.getParam('loggedWorkouts');
   const lastWorkout= allWorkouts[allWorkouts.length-1];
   const indexOfLastWorkout = daysArray.indexOf(lastWorkout.day);
   const stringOfNextWorkout = daysArray[(indexOfLastWorkout+1) % daysArray.length];
+  
+  let allWorkoutsDetails = []; 
+  
+  if(allWorkouts.length >= daysArray.length){
+    let i = daysArray.length;
+    let j = allWorkouts.length-1;
+    while(i > 0){
+      allWorkoutsDetails.push(allWorkouts[j])
+      i--;
+      j--;
+    }
+  }else{
+    let i = allWorkouts.length;
+    while (i > 0) {
+      allWorkoutsDetails.push(allWorkouts[i-1])
+      i--;
+    }
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -24,9 +35,7 @@ export default function WorkoutDetails({ navigation }) {
       <Text>Today is {stringOfNextWorkout}!</Text>
         <Card>
             <Text>{navigation.getParam('title')}</Text>
-            <SafeAreaView>
-                {listedDays}
-            </SafeAreaView>
+            <Text>{stringOfNextWorkout}</Text>
         </Card>
     </SafeAreaView>
   );
